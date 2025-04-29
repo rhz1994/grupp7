2738e5f9ec1ac84caa55b453bcdbcf9bd07997c8
Max Asensio <max.asensio13@gmail.com>
2025-04-29 13:34:06 +0200

    new endpoints, changes to app.js and gitignore

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,8 @@ const app = express();
 const port = 3000;
 
 const placesVisitedRoutes = require("./routes/placesVisitedRoutes");
-const continentsRoutes = require("./routes/continentsRoutes")
+const continentsRoutes = require("./routes/continentsRoutes");
+const countriesRoutes = require("./routes/countriesRoutes");
 
 app.use(express.json());
 
@@ -18,5 +19,7 @@ app.use(express.static("public"));
 app.use(placesVisitedRoutes);
 
 app.use(continentsRoutes);
+
+app.use(countriesRoutes);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
--- backend/services/countriesServices.js
+++ backend/services/countriesServices.js
@@ -1,5 +1,21 @@
 const connectionMySQL = require("../connectionMySQL");
 
+// CREATE
+function addCountry(countryName, countryCapital, countryPopulation) {
+    return new Promise((resolve, reject) => {
+        let sql = 'INSERT INTO countries (countryName, countryCapital, countryPopulation) VALUES (?,?,?)';
+        let params = [countryName, countryCapital, countryPopulation];
+
+        connectionMySQL.query(sql, params, (err) => {
+            if(err)
+                reject(err);
+            else
+                resolve();
+        });
+    });
+}
+
+// READ
 function getCountries() {
   return new Promise((resolve, reject) => {
     let sql = "SELECT * FROM countries";
@@ -9,6 +25,36 @@ function getCountries() {
     });
   });
 }
+
+// UPDATE
+function updateCountries(countryId, countryName, countryCapital, countryPopulation) {
+    return new Promise((resolve, reject) => {
+        let sql = 'UPDATE countries SET countryName = ?, countryCapital = ?, countryPopulation = ?, WHERE countryId = ?';
+        let params = [countryName, countryCapital, countryPopulation, countryId];
+
+        connectionMySQL.query(sql, params, (err) => {
+            if(err)
+                reject(err);
+            else
+                resolve();
+        });
+    });
+}
+
+// DELETE
+function deleteCountry(countryId) {
+    return new Promise((resolve, reject) => {
+        let sql = 'DELETE FROM countries WHERE countryId = ?';
+
+        connectionMySQL.query(sql, [countryId], (err) => {
+            if(err)
+                reject(err);
+            else
+                resolve();
+        });
+    });
+}
+
 module.exports = {
   getCountries,
 };
