058cf33b05af3b47e41aa946c3bb469f58352b66
Alfred Larsson - JSU24 GBG <alfred.larsson@iths.se>
2025-04-29 13:44:23 +0200

    added / on endpoint

--- backend/routes/continentsRoutes.js
+++ backend/routes/continentsRoutes.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const continentController = require('../controllers/continentsControllers');
 
 
-router.get('api/continents', continentController.getContinents);
-router.get('api/continent', continentController.getContinent);
+router.get('/api/continents', continentController.getContinents);
+router.get('/api/continent', continentController.getContinent);
 // router.post('api/continent', continentController.createContinent);
 // router.put('api/continent', continentController.updateContinent);
 // router.delete('api/continent', continentController.deleteContinent);
