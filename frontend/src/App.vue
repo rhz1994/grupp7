3543496e04de818cf529a62efde9cc96c4c24c77
Rasmus Hanzén <rhz1994@gmail.com>
2025-05-07 14:11:05 +0200

    worked on css

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -4,13 +4,6 @@ import HomeView from "./views/HomeView.vue";
 
 <template>
   <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-
     <HomeView />
   </div>
 </template>
